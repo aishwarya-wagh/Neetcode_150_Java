060c4dd3d6e9a2a4ae3910f570b0b0f533676042
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-05-24 19:14:44 -0400

    Easy problem

---
+++ majorityElement.java
@@ -0,0 +1,24 @@
+
+// https://leetcode.com/problems/majority-element/
+public class majorityElement {
+    public static int majorityElement(int[] nums) {
+        int count=0;
+        int candidate=0;
+        for(int n:nums){
+            if(count==0){
+                candidate=n;
+            }if(candidate==n){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        return candidate;
+
+    }
+public static void main(String[] args){
+        int[] input ={1,2,1,1,2,2,2};
+    System.out.println("majorityElement : "+majorityElement(input));
+}
+
+}
