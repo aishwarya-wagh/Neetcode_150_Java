6f0e6bc73b347105b93c70c0f3e3e46e6d30e851
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-06-14 21:53:13 -0400

    recursive solution

---
+++ climbingStairs.java
@@ -0,0 +1,12 @@
+public class climbingStairs {
+    //https://leetcode.com/problems/climbing-stairs/description/
+
+    public int climbStairs(int n) {
+        if(n==0 || n==1){
+            return 1;
+        }
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+
+
+}
