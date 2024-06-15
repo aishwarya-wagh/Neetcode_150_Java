399e9e43cb724789217793306c601900ff0c2ee0
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-06-15 10:08:32 -0400

    tabulation solution

--- climbingStairs.java
+++ climbingStairs.java
@@ -25,4 +25,18 @@ public class climbingStairs {
         }
         return memo.get(n);
     }
+
+    public int climbStairsTabulation(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n+1];
+        dp[0] = dp[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
 }
