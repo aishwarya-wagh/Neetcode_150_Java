ae5096b3c170992052657ee4bae52001dee43c9c
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-06-14 23:14:05 -0400

    memorization solution

--- climbingStairs.java
+++ climbingStairs.java
@@ -1,12 +1,28 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class climbingStairs {
     //https://leetcode.com/problems/climbing-stairs/description/
 
-    public int climbStairs(int n) {
+    public int climbStairsRecursion(int n) {
         if(n==0 || n==1){
             return 1;
         }
-        return climbStairs(n-1) + climbStairs(n-2);
+        return climbStairsRecursion(n-1) + climbStairsRecursion(n-2);
     }
 
+    public int climbStairsMemorization(int n) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return climbStairsMemorization(n, memo);
+    }
 
+    private int climbStairsMemorization(int n, Map<Integer, Integer> memo) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (!memo.containsKey(n)) {
+            memo.put(n, climbStairsMemorization(n-1, memo) + climbStairsMemorization(n-2, memo));
+        }
+        return memo.get(n);
+    }
 }
