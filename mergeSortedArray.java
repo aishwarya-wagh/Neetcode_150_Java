ca7d2437220f42a8b569a3d5527efe9627ad3fd1
Aishwarya-University-of-waterloo <aswagh@uwaterloo.ca>
2024-06-25 18:41:11 -0400

    alternative solution

--- mergeSortedArray.java
+++ mergeSortedArray.java
@@ -8,7 +8,7 @@ public class mergeSortedArray {
     int m=3;
     int n=3;
     int[] nums2 = new int[]{2,5,6};
-    mergeSortedArray.merge1(nums1,m,nums2,n);
+    mergeSortedArray.merge2(nums1,m,nums2,n);
     for(int nu: nums1)
         System.out.println(nu);
     }
@@ -23,6 +23,20 @@ public class mergeSortedArray {
         Arrays.sort(nums1);
     }
 
+    // complexity is O(m+n);
+    public static void merge2(int[] nums1, int m, int[] nums2, int n) {
+        int len1 = m - 1;
+        int len2 = n - 1;
+        int totalLen = m + n - 1;
+
+        while (len2 >= 0) {
+            if (len1 >= 0 && nums1[len1] > nums2[len2]) {
+                nums1[totalLen--] = nums1[len1--];
+            } else {
+                nums1[totalLen--] = nums2[len2--];
+            }
+        }
+    }
 
 
 }
